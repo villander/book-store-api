50097359ac1951a44303b0291604830cf2dc6456
Michael Villander <nousvillander@gmail.com>
2017-09-22 20:05:26 -0300

    Fix again

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import app from './config/express';
 
 app.set('port', (process.env.PORT || 3000));
 
-app.listen(3000, () => {
-  console.log('server started on port 3000');
+app.listen(app.get('port'), () => {
+  console.log(`server started on port ${app.get('port')}`);
 });
 
 export default app;
