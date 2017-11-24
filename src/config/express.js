aff58430bf8c9e41c5233c6a2e92d9b6f47b5915
Michael Villander <nousvillander@gmail.com>
2017-11-24 12:09:57 -0200

    Improve logs

--- src/config/express.js
+++ src/config/express.js
@@ -257,7 +257,7 @@ router.get('/api/books', validateRequest, (req, res) => {
       let url;
 
       if (user.google.favoriteInfo) {
-        console.log(user.google.favoriteInfo.volumeInfo);
+        console.log('favorito', user.google.favoriteInfo[0].volumeInfo);
         // const { favoriteInfo } = user.google;
         // let maxResults = (40 / favoriteInfo.length);
         // let startIndexInfo = (((Number(req.query.page)) * maxResults) - maxResults);
